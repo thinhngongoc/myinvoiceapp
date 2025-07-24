bcc9abefe551d694bcc95d8fd3e95ac2191b9f0f
thinhngongoc <thinhngobk@gmail.com>
2025-07-24 11:21:37 +0700

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -17,7 +17,7 @@ from backend.models import (
 # THAY ĐỔI DÒNG DATABASE_URL NÀY ĐỂ TRỎ ĐẾN SUPABASE TRANSACTION POOLER
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://postgres.izulhxhucaucqgfdxdwk:tK80A7QwSkgDN7f1@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres?sslmode=require"
+    "postgresql://postgres.izulhxhucaucqgfdxdwk:Baochau2024@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres?sslmode=require"
 )
 
 # Tạo engine cho PostgreSQL
